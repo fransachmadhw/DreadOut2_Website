db6c58f87e6b51701fad921ec7b50dc96062bfdf
frans <franswinata6@gmail.com>
2021-11-28 21:45:21 +0700

    Set custom scrollbar

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,26 +1,61 @@
-import Swiper from 'swiper/bundle';
+var swiper = new Swiper('.swiper', 
+      {
+        loop: true,
+        centeredSlides: true,
+        grabCursor: true,
+        autoplay: 
+        {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      });
 
-import 'swiper/css/bundle';
+      var swiper = new Swiper('.swiper-ghost', 
+      {
+        effect: 'fade',
+        loop: false,
+        centeredSlides: true,
+        grabCursor: true,
+        autoplay: 
+        {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      });
 
-const swiper = new Swiper('.swiper', 
-{
-    loop: true,
-    centeredSlides: true,
-    autoplay: 
-    {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-});
+      var swiper = new Swiper('.swiper-news', 
+      {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        mousewheel: true,
+        autoplay: 
+        {
+          delay: 2750,
+          disableOnInteraction: false,
+        },
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+      });
 
-const swiper = new Swiper('.swiper-ghost', 
-{
-    effect: 'fade',
-    loop: false,
-    centeredSlides: true,
-    autoplay: 
-    {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-});
+      var swiper = new Swiper('.swiper-hero', {
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        effect: 'fade',
+        speed: 500,
+        loop: true,
+        // pagination: {
+        //   el: '.swiper-pagination',
+        //   type: 'fraction',
+        // },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
