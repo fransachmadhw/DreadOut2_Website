68514308c4de7f90e7676aa43644795fa685bffc
frans <franswinata6@gmail.com>
2021-12-09 21:15:59 +0700

    Mobile navbar fixed

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,8 @@
-navbar = document.getElementById('navbar');
+const navbar = document.getElementById('navbar');
+const navbar2 = document.getElementById('navbar2');
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('.menu ul');
+const link = document.querySelector('.menu-list');
 
 window.addEventListener('scroll', function() 
 {
@@ -7,10 +11,23 @@ window.addEventListener('scroll', function()
     if(scrollTop > 75)
     {
         navbar.classList.add("nav-fixed");
+        navbar2.classList.add("nav-fixed");
     }
 
     else
     {
         navbar.classList.remove("nav-fixed");
+        navbar2.classList.remove("nav-fixed");
     }
 }) 
+
+menuToggle.addEventListener('click', function()
+{
+    nav.classList.toggle('slide');
+});
+
+link.addEventListener('click', function()
+{
+    nav.classList.toggle('slide');
+    menuToggle.checked = false;
+});
